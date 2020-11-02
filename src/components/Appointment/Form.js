5057b2154e7e3412222bc089a88b42725dd0c08f
Daniel <danschae@gmail.com>
2020-11-02 14:09:17 -0500

    add tests to form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,8 @@ export default function Form(props) {
 
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.value || null);
+  const [error, setError] = useState("");
+
 
   // used to reset the form
   const reset = () => {
@@ -16,6 +18,16 @@ export default function Form(props) {
   const cancel = () => {
     reset()
     props.onCancel()
+  }
+
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    setError("");  
+    props.onSave(name, interviewer);
   }
 
 
@@ -29,17 +41,19 @@ export default function Form(props) {
         type="text"
         placeholder="Enter Student Name"
         onChange={(event) => setName(event.target.value)}
+        data-testid="student-name-input"
         /*
           This must be a controlled component
         */
       />
     </form>
+    <section className="appointment__validation">{error}</section>
     <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} /> 
   </section>
   <section className="appointment__card-right">
     <section className="appointment__actions">
       <Button danger onClick={cancel}>Cancel</Button>
-      <Button confirm onClick={() => {props.onSave(name, interviewer)}}>Save</Button>
+      <Button confirm onClick={() => {validate()}}>Save</Button>
     </section>
   </section>
 </main>
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,14 @@
 import React from "react";
 import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
+import PropTypes from 'prop-types';
 
-export default function InterviewList(props) {
+
+export default function InterviewerList(props) {
+  
+  InterviewerList.propTypes = {
+    interviewers: PropTypes.array.isRequired
+  };
   
   const interviewers = props.interviewers.map(interviewer => {
     
